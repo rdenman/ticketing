fc50acd724ea3e07ebaee291d661eca8b14702ca
Rob Denman <robertkarldenman@gmail.com>
2021-01-17 14:40:03 -0600

    add common lib

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,9 +1,8 @@
+import { errorHandler, NotFoundError } from "@rdtix/common";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import express from "express";
 import "express-async-errors";
-import { NotFoundError } from "./errors/not-found-error";
-import { errorHandler } from "./middlewares/error-handler";
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/sign-in";
 import { signOutRouter } from "./routes/sign-out";
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
+import { currentUser } from "@rdtix/common";
 import express from "express";
-import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
